2bed62e6d7b0d83f774dc059e1b2b7de88869284
Enrico Dreyer <enrico.livex.co.za>
2023-06-06 12:15:36 +0200

    UI Design and CRUD Services

--- ClientApp/src/app/phone-book/phone-book.component.ts
+++ ClientApp/src/app/phone-book/phone-book.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppService } from '../app.service';
+import { PhoneBookEntry } from '../models/phone-book-entry';
 
 @Component({
   selector: 'app-phone-book',
@@ -7,15 +10,28 @@ import { Component, Inject } from '@angular/core';
   styleUrls: ['./phone-book.component.css']
 })
 export class PhoneBookComponent {
+  phoneBookEntry: PhoneBookEntry[] = [];
 
-  firstName: string = "";
-  surname: string = "";
-  phoneNumber: string = "";
 
   constructor(
     private http: HttpClient,
-    @Inject('BASE_URL') private baseUrl: string
+    private appservice: AppService,
+    // @Inject('BASE_URL') private baseUrl: string
   ) {
+    
+  }
+  ngOnInit(): void {
+    // fetch Students
+    this.appservice.GetAllEntries()
+    .subscribe(                               
+      (successResponse) => {
+
+       this.phoneBookEntry = successResponse 
+      },
+      (errorResponse) => {
+        console.log(errorResponse);
+      }
+    );
   }
 }
 
