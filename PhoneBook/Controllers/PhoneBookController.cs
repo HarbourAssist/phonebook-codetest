e63b1f6ea909f52540abb45f6183b454e995070b
jacca <jacca@DESKTOP-RDVIJKS>
2023-10-03 21:24:26 +0100

    Added CRUD endpoints to phone book API

--- PhoneBook/Controllers/PhoneBookController.cs
+++ PhoneBook/Controllers/PhoneBookController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PhoneBook.Models;
+using PhoneBook.Models.Input;
 
 namespace PhoneBook.Controllers
 {
@@ -7,13 +8,61 @@ namespace PhoneBook.Controllers
     [Route("api/[controller]")]
     public class PhoneBookController : ControllerBase
     {
-        [HttpGet]
-        public List<PhoneBookEntry> GetAll()
+        private readonly PhoneBookContext _phoneBookContext;
+
+        public PhoneBookController(PhoneBookContext phoneBookContext)
         {
-            return new List<PhoneBookEntry> { 
-                new PhoneBookEntry { PhoneBookEntryId = 1, Firstname = "Jacca", Surname = "Hooper", PhoneNumber = "01234567890" },
-                new PhoneBookEntry { PhoneBookEntryId = 1, Firstname = "Joe", Surname = "Bloggs", PhoneNumber = "98765432101" },
+            _phoneBookContext = phoneBookContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<PhoneBookEntry> GetAll()
+        {
+            var phoneBookEntries = _phoneBookContext.PhoneBookEntries;
+            return phoneBookEntries;
+        }
+
+        [HttpPost]
+        public PhoneBookEntry Create(NewPhoneBookEntry newPhoneBookEntry)
+        {
+            var phoneBookEntry = new PhoneBookEntry
+            {
+                Firstname = newPhoneBookEntry.Firstname,
+                Surname = newPhoneBookEntry.Surname,
+                PhoneNumber = newPhoneBookEntry.PhoneNumber,
             };
+            _phoneBookContext.PhoneBookEntries.Add(phoneBookEntry);
+            _phoneBookContext.SaveChanges();
+            
+            return phoneBookEntry;
+        }
+
+        [HttpPatch("{id}")]
+        public PhoneBookEntry Update(int id, PhoneBookEntry updatedPhoneBookEntry)
+        {
+            var entry = _phoneBookContext.PhoneBookEntries.SingleOrDefault(p => p.PhoneBookEntryId == id);
+
+            if (entry == null)
+                throw new ArgumentOutOfRangeException($"Could not find phone book entry {nameof(updatedPhoneBookEntry.PhoneBookEntryId)}={updatedPhoneBookEntry.PhoneBookEntryId}");
+
+            entry.Firstname = updatedPhoneBookEntry.Firstname;
+            entry.Surname = updatedPhoneBookEntry.Surname;
+            entry.PhoneNumber = updatedPhoneBookEntry.PhoneNumber;
+            
+            _phoneBookContext.SaveChanges();
+            return entry;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var entry = _phoneBookContext.PhoneBookEntries.SingleOrDefault(p => p.PhoneBookEntryId == id);
+
+            if (entry == null)
+                throw new ArgumentOutOfRangeException($"Could not find phone book entry with {nameof(id)}={id}");
+
+            _phoneBookContext.PhoneBookEntries.Remove(entry);
+            _phoneBookContext.SaveChanges();
         }
     }
 }
---
+++ PhoneBook/Models/Input/NewPhoneBookEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBook.Models.Input
+{
+    public class NewPhoneBookEntry
+    {
+        public string Firstname { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
